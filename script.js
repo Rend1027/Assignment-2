ab12a7ff9170f016860b8c41b5a91ef2d34bc980
bryan-zhong46 <bryan.zhong46@stu.bmcc.cuny.edu>
2025-06-17 14:29:58 -0400

    Added myIndexOf() method

--- script.js
+++ script.js
@@ -8,3 +8,17 @@ function myIncludes(arr, value) {
   }
   return present;
 }
+
+// Returns the index of the first occurence of value in array, starting from fromIndex, returns -1 if value is not present in array.
+// If unspecified, fromIndex is 0.
+function myIndexOf(arr,value,fromIndex=0) {
+  let location = -1;
+  let i = fromIndex;
+  while (location === -1 && i < arr.length) {
+    if (arr[i] === value) {
+      location = i;
+    }
+    i++;
+  }
+ return location;
+}
