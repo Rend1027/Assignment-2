58722ae075e9ed8e2437f376bc92e96c3b0f6fe6
bryan-zhong46 <bryan.zhong46@stu.bmcc.cuny.edu>
2025-06-17 15:40:23 -0400

    Added lastIndexOf() method

--- script.js
+++ script.js
@@ -22,3 +22,26 @@ function myIndexOf(arr,value,fromIndex=0) {
   }
  return location;
 }
+
+// Appends elements to the end of the given array and returns the length of the new array. Multiple elements can be added.
+function myPush(arr,elem1,elem2,elem3) {
+  for (let i = 1; i < arguments.length; i++) {
+    arr[arr.length] = arguments[i];
+  }
+  return arr.length;
+}
+
+// Returns the index of the last occurence of value in array. Returns -1 if value is not present in array.
+function myLastIndexOf(arr,value) {
+  let location = -1;
+  let i = arr.length - 1;
+  while (location === -1 && i > -1) {
+    if (arr[i] === value) {
+      location = i;
+    }
+    i--;
+  }
+ return location;
+}
+
+
